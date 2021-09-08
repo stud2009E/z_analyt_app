c2dec0adad382ec482793623769e35d437092513
18547995 <18547995@sigma.sbrf.ru>
2021-09-08 18:14:33 +0300

    [FEATURE] add visual filter

---
+++ webapp/ext/controller/AnalyticalListPageExt.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define([], function(){
+	"use strict";
+	
+	return sap.ui.controller("z.anlt.app.ext.controller.AnalyticalListPageExt", {
+		onInit: function(){
+			
+			//here goes your code
+
+		},
+
+		onInitSmartFilterBarExtension: function(oEvent){
+
+		},
+
+		onBeforeRebindChartExtension: function(oEvent){
+
+		},
+
+		onBeforeRebindVisualFilterExtension: function(oEvent){
+
+		}
+
+	});
+
+});
--- webapp/ext/controller/ListReportExt.controller.js
+++
@@ -1,7 +0,0 @@
-sap.ui.controller("z.anlt.app.ext.controller.ListReportExt", {
-	onInit: function(){
-		
-		//here goes your code
-
-	}
-});
